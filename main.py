a76ad15ecbc5ea383c4923846ff49550f0c4f4e0
supergebulinKing <2405593450@qq.com>
2026-06-08 00:29:59 +0800

    结构化输出以及模板套用

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ from langchain_deepseek import ChatDeepSeek
 from langchain_core.prompts import ChatPromptTemplate           
 from langchain_core.output_parsers import StrOutputParser        
 
-llm = ChatDeepSeek(model = "deepseek-v4-flash",temperature=0.7)
+llm = ChatDeepSeek(model = "deepseek-v4-flash",temperature=0.1)
 prompt = ChatPromptTemplate.from_messages([
     ("system" ,"你是一个全能型的助手"),
     ("human" , "请解释是{topic}")
@@ -10,5 +10,5 @@ prompt = ChatPromptTemplate.from_messages([
 
 chain  = prompt | llm | StrOutputParser()
 
-response = chain.invoke({"topic": "Java程序设计"})
+response = chain.ainvoke({"topic": "Java程序设计"})
 print(response)
